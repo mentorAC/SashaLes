7a67fc6b0da40fa6f78fab7c20011cad1ad1c27e
maxpe3447 <121520054+mentorAC@users.noreply.github.com>
2023-12-15 17:53:46 +0200

    update project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { RedCub } from './RedCub/RedCub';
+import { MyButton } from './RedCub/button/button';
 function App() {
+
+  let text = "hello react";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <div className='container'>
+    {/* <RedCub></RedCub> */}
+    <RedCub text="my text 1" count="4" second_text="hi"></RedCub>
+    <RedCub text="qwert" second_text="hi!"></RedCub>
+    <RedCub text="my text 3"></RedCub>
+    <MyButton text="click"></MyButton>
+    <MyButton text="dont click"></MyButton>
+    <MyButton text="click 2"></MyButton>
+    <MyButton></MyButton>
+    <MyButton></MyButton>
+    <MyButton></MyButton>
+   </div>
+    ) ;
 }
 
 export default App;
